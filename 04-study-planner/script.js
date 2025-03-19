e099b6cd3f57f7cbf969e543d0222b92a17c7fab
beansebrrr <173890355+beansebrrr@users.noreply.github.com>
2025-03-19 18:16:08 +0400

    ✨ Create todo-list ✅ and notes 📓 section

--- 04-study-planner/script.js
+++ 04-study-planner/script.js
@@ -1,1 +1,25 @@
-console.log("yep.");
+const Tasks = document.querySelectorAll("#todo-list .task")
+
+const subTasks = document.querySelectorAll("#todo-list .sub-task")
+
+/* Main task checkboxes */
+Tasks.forEach(task => {
+  const checkbox = task.querySelector(".checkbox")
+  checkbox.addEventListener("click", () => {
+    task.classList.toggle("checked")
+  })
+})
+
+/* Subtask checkboxes */
+subTasks.forEach(subTask => {
+  subTask.addEventListener("dblclick", () => {
+    subTask.classList.toggle("checked")
+  })
+})
+
+/* Cloning the notes to speed things up. */
+const note = document.querySelector(".note")
+
+for (let i = 0; i < 10; i++) {
+  note.parentElement.appendChild(note.cloneNode(true))
+}
