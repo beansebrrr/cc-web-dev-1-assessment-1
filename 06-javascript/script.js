eaee9b7f19c9a38b2fd5a40b41743b5da940e8a4
beansebrrr <173890355+beansebrrr@users.noreply.github.com>
2025-03-22 20:12:42 +0400

    🎨 Polish and Styling

--- 06-javascript/script.js
+++ 06-javascript/script.js
@@ -10,33 +10,42 @@ function fieldAndValue(selector) {
     this.value = this.field.value;
   })
 
-  // Lets me set a default value
-  this.setDefaultValue = (defaultValue) => {
-    this.value = this.field.value = defaultValue;
+  // Lets me set a value through code
+  this.setValue = (newValue) => {
+    this.value = this.field.value = newValue;
   };
 }
 
 /**
  * Set up fields and their defaults here
  */
-
 const costPerLitre = new fieldAndValue("#cost-per-litre");
-costPerLitre.setDefaultValue(1.72)
+costPerLitre.setValue(1.72)
 const numOfLitres = new fieldAndValue("#litres");
-numOfLitres.setDefaultValue(0)
+numOfLitres.setValue(0)
 
-const submitBtn = document.querySelector("button#evaluate");
+// Elements that are NOT fields but I still need
+const calculatorForm = document.querySelector("form#petrol-calculator")
 const outputText = document.querySelector("#output")
 
+// Unicode emojis (not the most important but I'm defining them here now)
+const sparkleEmoji = String.fromCodePoint(0x2728)
+const warningEmoji = String.fromCodePoint(0x26A0,0xFE0F)
 
 /**
  * Functionalities go here
  */
-
-submitBtn.addEventListener("click", event => {
+calculatorForm.addEventListener("submit", event => {
   event.preventDefault();
   const totalPrice = calculateGasPrice(costPerLitre.value, numOfLitres.value);
-  outputText.textContent = `AED ${totalPrice}`
+  
+  // Will print out an error message if the output is invalid
+  const result = !Number.isNaN(totalPrice)
+  ? `${sparkleEmoji} ${totalPrice.toLocaleString('en', {style:"currency", currency:"AED"})} ${sparkleEmoji}`  // Currency Formatting.
+  : `${warningEmoji} Error ${warningEmoji}`;  // Error message if all else fails.
+
+  // Display result to the HTML Document
+  outputText.textContent = result;
 })
 
 const calculateGasPrice = (cost, quantity) => {
