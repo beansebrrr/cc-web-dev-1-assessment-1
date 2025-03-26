c80e393b2f27eb23271565caff2431a0c618384e
beansebrrr <173890355+beansebrrr@users.noreply.github.com>
2025-03-26 20:09:08 +0400

    🎨 Final Polish

--- 05-soundboard/text-to-speech.js
+++ 05-soundboard/text-to-speech.js
@@ -1,14 +1,22 @@
+/* Select the relevant elements for this to work */
 const textField = document.querySelector("#text-to-speech>textarea")
 const sayBtn = document.querySelector("#text-to-speech>#say")
+
+/* Web Speech API */
 const TTS = new SpeechSynthesisUtterance();
 
 sayBtn.addEventListener("click", () => {
+  // Extract audio from textfield, says "hello world" if blank
   TTS.text = textField.value || "Hello, world!"
+  // Speak.
   window.speechSynthesis.speak(TTS)
+
+  // Styling
   sayBtn.classList.add("audio-active")
   sayBtn.setAttribute("disabled", "")
 })
 
+// Also styling
 TTS.addEventListener("end", () => {
   sayBtn.classList.remove("audio-active")
   sayBtn.removeAttribute("disabled")
